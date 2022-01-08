b833a196f6bf110b0c65900de3e4420efa872d12
unknown <andreyxdd@yandex.ru>
2022-01-07 23:44:06 -0700

    Added heroku pinger

---
+++ pinger.py
@@ -0,0 +1,22 @@
+"""
+Script to ping a web-resource by the PING_URL environmental variable.
+It is par for the course approach to keep apps hosted on heroku from sleeping.
+The script is called from the heroku scheduler worker. See dashboard fro details.
+"""
+import os
+from datetime import datetime
+import requests
+
+from utils.handle_emails import notify_developer
+
+try:
+    response = requests.get(os.getenv("PING_URL"))
+    print(f"pinger.py responded with: {response}")
+except Exception as e:  # pylint: disable=W0703
+    print(f"pinger.py reported an error: {e}")
+    notify_developer(
+        body=(
+            f"Pinger reported an error at {datetime.utcnow()} UTC time."
+            + "Something went wrong: \n{e}"
+        )
+    )
