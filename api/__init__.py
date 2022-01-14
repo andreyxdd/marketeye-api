8998a8820fbd2d1ecbf6cdc0158cd2ce1074a610
unknown <andreyxdd@yandex.ru>
2022-01-14 15:21:05 -0700

    Implemented new route and an endpoint to send notifications to the developer via email

--- api/__init__.py
+++ api/__init__.py
@@ -6,8 +6,10 @@ from fastapi import APIRouter
 
 from .endpoints.tests import tests_router
 from .endpoints.analytics import analytics_router
+from .endpoints.notifications import notifications_router
 
 router = APIRouter()
 
 router.include_router(tests_router, prefix="/tests")
 router.include_router(analytics_router, prefix="/analytics")
+router.include_router(notifications_router, prefix="/notifications")
---
+++ api/endpoints/notifications.py
@@ -0,0 +1,56 @@
+"""
+Endpoints to make email notifications
+"""
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import Response
+from pydantic import BaseModel
+
+from core.settings import API_KEY
+from utils.handle_emails import notify_developer
+
+notifications_router = APIRouter()
+
+
+class Notification(BaseModel):  # pylint: disable=R0903
+    """
+    Class representing notifications request body
+    """
+
+    email_body: str
+
+
+@notifications_router.get("/")
+async def home():
+    """
+    Initial notifications route
+
+    Returns:
+
+        Response: welcome sign
+    """
+    return Response("Hello World! It's a Notifications Router")
+
+
+@notifications_router.post("/notify_developer")
+async def run_developer_notification(notification: Notification, api_key: str):
+    """[summary]
+
+    Args:
+        notification (Notification): Notifications body object
+        api_key (str): key to allow/disallow a request
+
+    Raises:
+        HTTPException: Incorrect API key provided
+        HTTPException: Notifications has not been sent due to internal error
+
+    Returns:
+        dict: {"detail": str}
+    """
+    if api_key != API_KEY:
+        raise HTTPException(status_code=400, detail="Erreneous API key recieved.")
+
+    try:
+        notify_developer(body=notification.email_body)
+        return {"detail": "Notifications has been sent successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=503, detail=e) from e
